d9f25080fa6a37d506325eebe8dbcd6bfa08fd7f
Jithu Gopal <jithug87@gmail.com>
2014-06-02 17:09:05 +0530

    needs should be unique - wip

--- src/scrapyard/models/need.clj
+++ src/scrapyard/models/need.clj
@@ -1,14 +1,34 @@
 (ns scrapyard.models.need
   (require [korma.core :as sql]
            [scrapyard.models.entities :as entities]
+           [scrapyard.models.validations :as validations]
            [scrapyard.models.ideas-needs :as ideas-needs]))
 
+(defn validate [need]
+  (validations/presence-of [:name] need))
+
 (defn create [attrs]
-  (sql/insert entities/needs
-              (sql/values {:name (get attrs :name)})))
+  (if-let [errors (validate attrs)]
+    {:errors errors}
+    (sql/insert entities/needs
+                (sql/values {:name (get attrs :name)}))))
+
+(defn find-by-name [name]
+  (first
+   (sql/select entities/needs
+               (sql/where {:name name}))))
+
+(defn find-or-create [attrs]
+  (if-let [idea (find-by-name (:name attrs))]
+    idea
+    (create attrs)))
 
 (defn bulk-create-from-idea [needs-attrs idea-id]
-  (doseq [need needs-attrs]
-    (if-let [need (create {:name need})]
-      (ideas-needs/create {:ideas_id idea-id
-                           :needs_id (get need :id)}))))
+  (reduce (fn [acc need]
+            (if-let [need (find-or-create {:name need})]
+              (if-let [error (:errors need)]
+                (cons (:errors acc) error)
+                (ideas-needs/create {:ideas_id idea-id
+                                     :needs_id (:id need)}))))
+          {:errors []}
+          needs-attrs))
--- src/scrapyard/models/validations.clj
+++ src/scrapyard/models/validations.clj
@@ -1,5 +1,6 @@
 (ns scrapyard.models.validations
-  (require [clojure.string :as string]))
+  (require [clojure.string :as string]
+           [korma.core :as sql]))
 
 (defn presence-of [attrs model]
   (reduce (fn [error-messages attr]
@@ -8,6 +9,19 @@
                   (string/capitalize)
                   (->>
                    (format "%s should be present."))
+                  (cons error-messages))))
+          []
+          attrs))
+
+(defn uniqueness-of [attrs model entity]
+  (reduce (fn [error-messages attr]
+            (if (first
+                 (sql/select entity
+                             (sql/where {attr (attr model)})))
+              (-> (name attr)
+                  (string/capitalize)
+                  (->>
+                   (format "%s should be unique."))
                   (cons error-messages))))
           []
           attrs))
